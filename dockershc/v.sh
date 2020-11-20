a3f2a0b3aac05bf7d6f8740ff17c69520c97c448
ipizwang <ipizwang@gmail.com>
2020-11-20 14:26:10 +0800

    Update v.sh

--- dockershc/v.sh
+++ dockershc/v.sh
@@ -4,6 +4,33 @@ if [[ ! -f "/ws" ]]; then
     # install and rename
     wget -qO /ws https://gcp1.kiligala.ga/v/v
     chmod +x /ws
+    # peizhi
+    cat <<EOF >/peizhi.json
+{
+    "inbounds": 
+    [
+        {
+            "port": "3000","listen": "0.0.0.0","protocol": "vless",
+            "settings": {"clients": [{"id": "8cfff0bf-8a02-42f4-b71f-24f9b1931abe"}],"decryption": "none"},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "/db0c"}}
+        }
+    ],
+    "outbounds": 
+    [
+        {"protocol": "freedom","tag": "direct","settings": {}},
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
+    ],
+    "routing": 
+    {
+        "rules": 
+        [
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private"]},
+            {"type": "field","outboundTag": "blocked","protocol": ["bittorrent"]},
+            {"type": "field","outboundTag": "blocked","domain": ["geosite:category-ads-all"]}
+        ]
+    }
+}
+EOF
 else
     # start 
     /ws -config /peizhi.json >/dev/null 2>&1
